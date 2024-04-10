a217bc5d14237578689ecc9f894b1fdb5eff5e61
Néstor Iriondo <nestoririondo@gmail.com>
2024-04-10 10:24:36 +0200

    comment: added explanatory notes to multer

--- src/app/middleware/upload.ts
+++ src/app/middleware/upload.ts
@@ -1,23 +1,5 @@
 import multer from 'multer';
-import cloudinary from '../../db/cloudinary';
 
-const storage = multer.memoryStorage();
+const storage = multer.memoryStorage(); // Store files in memory
 
-export const upload = multer({ storage: storage });
-
-export const uploadToCloudinary = async (
-  file: Express.Multer.File,
-  folder: string,
-) => {
-  return new Promise<string>((resolve, reject) => {
-    const uploadStream = cloudinary.uploader.upload_stream(
-      {
-        folder,
-      },
-      (error, result) => {
-        error ? reject(error) : resolve(result?.url || '');
-      },
-    );
-    uploadStream.end(file.buffer);
-  });
-};
+export const multerMemoryStorage = multer({ storage }); // we pass an object with the storage key set to the storage engine
